56592e939bd624f96732fa77760a1a93422b66ff
pman-me <0xpman-me@proton.me>
2024-11-24 19:00:55 +0100

    separating different db functionalities from each other

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from common.consts import chain_data
+from rdbms_utils.create_db import create_db
+from rdbms_utils.engine import get_db_engine
 from transaction.analyze_transaction_type import categorize_transaction
 from transaction.txs_data_per_chain import fetch_txs_per_chain
 
 
 def main():
-
+    create_db(get_db_engine())
     categorize_transaction(chain_data, fetch_txs_per_chain())
 
 
--- rdbms_utils/create_tables.py
+++ rdbms_utils/create_tables.py
@@ -5,7 +5,3 @@ from rdbms_utils.sql_base import SQLBASE
 def create_tables(db_engine):
     import models.transaction_model
     SQLBASE.metadata.create_all(db_engine)
-
-
-if __name__ == '__main__':
-    create_tables(get_db_engine())
